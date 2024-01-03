072136773ec00dbcca6ef8297223c0728a280d86
unknown <upendrapanchali83@gmail.com>
2024-01-03 13:06:07 +0530

    completed the addto cart page

--- src/Features/Addtocart/Addtocart.js
+++ src/Features/Addtocart/Addtocart.js
@@ -9,12 +9,47 @@ import {ProductsData} from '../../ProductsData/ProductsData';
 import atcimg from '../../assests/addtocart.png';
 import { useNavigate } from 'react-router-dom';
 
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+
 function Addtocart(){
 
     const [cartkeys,setcartkeys] = useState([]);
+    const [quantity,setquantity] = useState(0);
     const sharedvalue = useContext(MyContext); // importing the sharedvalue
 
+    //size selection value 
+    // const [csize,setcsize] = useState("");
+
     const navigate = useNavigate();
+
+    // function subtraction
+    function decquan(){
+        if(quantity>1){
+            setquantity(prev=> prev-1);
+        }
+    }
+    //function addition
+    function incquan(){
+        setquantity(prev=> prev+1);
+    }
+
+    //function number with commas
+    function numberwithcommas(x){
+        let z=x;
+        z=Math.floor(z);
+        return z.toString().replace(/\B(?=(\d{3})+(?!\d))/g,",");
+    }
+
+    function funcaldis(d,p){
+        let x = Number(d);
+        let p1=Number(p);
+        let n1=x*p1;
+        n1=n1/100;
+        p1=p1-n1;
+        return p1;
+
+    }
+
     
     useEffect(() => {
         if (sharedvalue && sharedvalue.cart && sharedvalue.cart[0]) {
@@ -28,7 +63,7 @@ function Addtocart(){
             <img src={Emptycart} alt='emptycart'/>
         </div>
         :
-        <div>
+        <div className='addtocart-second-con'>
             <div className='addtocart-first-img'>
                 <img src={atcimg} alt='addtocart'/>
             </div>
@@ -36,13 +71,47 @@ function Addtocart(){
                 <h1>Cart</h1>
                 <p><span onClick={()=>navigate('/shop')}>Home / Shop /</span> Cart</p>
             </div>
+            <div className='addtocart-all-items'>
             {ProductsData.filter((item)=>cartkeys.includes(JSON.stringify(item.id))).map((product,idx)=>(
-                <div key={idx}>
+                <div key={idx} className='addtocart-each-item'>
+                    <div className='addtocart-ei-img'>
                     <img src={product.imgurl} alt='product-pic'/>
-                    <h1>{sharedvalue.cart[0][product.id].size}</h1>
-                    <h2>{sharedvalue.cart[0][product.id].qty}</h2>
+                    </div>
+                    <div className='addtocart-eachitem-details'>
+                        <h1>{product.name}</h1>
+                        <p>Rs.{Number(product.dis)>0 && Number(product.dis)<=99?<span className='with-dis'>{numberwithcommas(funcaldis(product.dis,product.price))}<span className='dismiss-price'>{product.price}</span> <span className='percent-off-red'>({product.dis}% OFF)</span></span>:<span className='not-dis'>{numberwithcommas(product.price)}/-</span>}</p>
+                        <div className='addtocart-size-qty'>
+                            <div className='addtocart-size-seclection'>
+                                <label>Size</label>
+                                <select value={sharedvalue.cart[0][product.id].size} >
+                                    <option value="">Select</option>
+                                    <option value="xs">XS</option>
+                                    <option value="s">S</option>
+                                    <option value="m">M</option>
+                                    <option value="l">L</option>
+                                    <option value="xl">XL</option>
+                                    <option value="xxl">XXL</option>
+                                    <option value="xxxl">XXXL</option>
+                                </select>
+                            </div>
+                            <div className='eachitem-quantity'>
+                                <p>quantity</p>
+                                    <div className='eachitem-quantity-paras'>
+                                        <p onClick={()=>decquan()}>-</p>
+                                        <p>{sharedvalue.cart[0][product.id].qty}</p>
+                                        <p onClick={()=>incquan()}>+</p>
+                                    </div>      
+                            </div>
+                        </div>
+                        <div className='addtocart-remove-item'>
+                            <DeleteOutlineOutlinedIcon sx={{color:'red'}}/>
+                            <p>Remove Item</p>
+                        </div>
+                    </div>
                 </div>
             ))}
+            </div>
+            
         </div>
         }
             
